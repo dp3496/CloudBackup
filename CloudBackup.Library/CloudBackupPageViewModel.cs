2fd279b4a99d94f6f7b5900a9ce00127cf06519d
930QCA <930QCA@DESKTOP-1T98ENO>
2021-09-23 06:03:14 -0700

    Add project files.

---
+++ CloudBackup.Library/CloudBackupPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CloudBackup.Library
+{
+    public class CloudBackupPageViewModel
+    {
+        public CloudBackupPageViewModel()
+        {
+            AutoBackupCommand = new CommandViewModel(obj => true, Execute);
+        }
+        public IList<string> BackupOptions { get; set; } = new List<string>
+        {
+            "Google",
+            "DropBox"
+        };
+
+        public ICommand AutoBackupCommand { get; set; }
+
+        private void Execute(object parameter)
+        {
+
+        }
+    }
+
+    public class CommandViewModel : ICommand
+    {
+        private readonly Predicate<object> canExecute;
+        private readonly Action<object> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public CommandViewModel(Predicate<object> canExecute, Action<object> execute)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ CloudBackup.Library/OneDriveApiConnect.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.OneDrive.Sdk;
+using Microsoft.OneDrive.Sdk.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudBackup.Library
+{
+    public class OneDriveApiConnect
+    {
+        private const string RedirectUrl = "https://login.live.com/oauth20_desktop.srf";
+
+        private const string ClientId = "0144b957-ed5c-499d-bd03-091db622798e";
+
+        private const string Scope = "onedrive.readwrite";
+
+
+        public async void GetDriveClient()
+        {
+            var msaAuthProvider = new MsaAuthenticationProvider(ClientId, RedirectUrl, new string[] { Scope, "wl.signin" });
+            await msaAuthProvider.AuthenticateUserAsync();
+            var oneDriveClient = new OneDriveClient(@"https://api.onedrive.com/v1.0", msaAuthProvider);
+        }
+    }
+}
